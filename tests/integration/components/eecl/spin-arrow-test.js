8313fa08895a43e661c508f32f9f7a22356851c3
Alex Maingot <alex.maingot@gmail.com>
2019-05-24 17:11:24 -0500

    Adding an svg to the component

--- tests/integration/components/eecl/spin-arrow-test.js
+++ tests/integration/components/eecl/spin-arrow-test.js
@@ -1,26 +1,14 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | eecl/spin-arrow', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{eecl/spin-arrow}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#eecl/spin-arrow}}
-        template block text
-      {{/eecl/spin-arrow}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), '');
   });
 });
