121a34193cdc7900b69373ec3cd71589d8682b98
Alex Maingot <alexmaingot@scalefactor.com>
2019-05-30 14:16:36 -0500

    Adding danger file PR title checker

--- addon/components/eecl/spin-arrow.ts
+++ addon/components/eecl/spin-arrow.ts
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 import layout from 'example-ember-component-library/templates/components/eecl/spin-arrow';
 
 export default Component.extend({
-  layout
+  layout,
 });
--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,28 @@
-import {message, danger} from "danger"
+import { fail, message, danger } from 'danger';
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+const modifiedMD = danger.git.modified_files.join('- ');
+message('Changed Files in this PR: \n - ' + modifiedMD);
+
+const commitTypes = [
+  'feat',
+  'fix',
+  'docs',
+  'style',
+  'refactor',
+  'perf',
+  'test',
+  'chore',
+];
+
+const components = ['spin-arrow'];
+
+const prTitleRegex = new RegExp(
+  '^(' + commitTypes.join('|') + ')(' + components.join('|') + '||):s.*$'
+);
+const correctlyFormattedPrTitle = danger.github.pr.title.match(prTitleRegex);
+
+if (correctlyFormattedPrTitle === null) {
+  fail('Your PR title should be correctly formatted.');
+} else {
+  message('Your PR title is correctly formatted! Good job!');
+}
