dc1f28d6c39a6d7c7e859b078a222c44ed0ec8e0
Alex Maingot <alexmaingot@scalefactor.com>
2019-05-30 14:43:44 -0500

    Annother attempt to fix danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,6 +3,11 @@ import { fail, message, danger } from 'danger';
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
 
+// eslint-disable-next-line no-undef
+const branchName = process.env.CIRCLE_BRANCH;
+message(`View the new documentation for this branch by visiting this site:
+https://ember-ui-lib.hmm.dev/versions/${branchName}/`);
+
 const commitTypes = [
   'feat',
   'fix',
@@ -22,7 +27,9 @@ const prTitleRegex = new RegExp(
 const correctlyFormattedPrTitle = danger.github.pr.title.match(prTitleRegex);
 
 if (correctlyFormattedPrTitle === null) {
-  fail('Your PR title should be correctly formatted.');
+  fail(`Your PR title should be correctly formatted. Your PR title must follow this regex
+    ${prTitleRegex.source}
+  `);
 } else {
   message('Your PR title is correctly formatted! Good job!');
 }
